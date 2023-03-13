1e7cb129d6527e1c12975c2025edb809214460a2
Carlos <ESTuGraz2023@gmail.com>
2023-03-13 10:09:02 -0700

    Sound melodic

--- ArduinoT.ino
+++ ArduinoT.ino
@@ -1,16 +1,14 @@
-
 //CTE BUZZER
 //Number of pin that we are using in the microcontroller
 const int buzzer = 8 ; //DigitalOut
 
-
-
 //CTE SOUL MOISTURE SENSOR
 const int soil= A5 ;  // Analog IN > If you have question about why Analog In and not Digital Out ask to Carlos
-
+//Var for reading info
 int soil_Moisture_sensor_INFO = 0; //https://www.arduino.cc/reference/en/language/functions/analog-io/analogread/
-// C++ code
-//
+
+
+
 void setup()
 {
 
@@ -18,8 +16,6 @@ void setup()
 //Here you must say how your pin will act, OUTPUT or INPUT > Buzzer=> I need to give electricity to it, OUTPUT
   pinMode(buzzer,OUTPUT);
   
-
-
 //Definition SOUL> INPUT
 pinMode(soil,INPUT); 
 //Why ? I wanna see the information that the microcontroller is reading in my computer
@@ -29,34 +25,49 @@ Serial.begin(9600);
 void loop()
 {
 
-/*
+
 //START SOUL MOISTURE
   soil_Moisture_sensor_INFO  = analogRead(soil);
 	Serial.println(soil_Moisture_sensor_INFO); 
   if(soil_Moisture_sensor_INFO<100){
 
-tone(buzzer, 600);
-delay (3000); //Time that I will execute the last line of code
+ //START CODE BUZZER
+	int i=0;
+  do{
+	i++;
+  //tone(pin, frequency)=> Execute a sound with the frecuency per the pin that I passed
+tone(buzzer, 450);
+delay (200); //Time that I will execute the last line of code
 //Stop the signal > sound for the time that I will specific in the delay
 noTone(buzzer);
-delay (1000);
+delay (200); //Time that I will execute the las time of code
+  
+  }while(i<2);
+ delay(3000);
+	// END CODE BUZZER
 	
   }else {
     
 }
- */
+ 
 
-
+/*
   //START CODE BUZZER
+	int i=0;
+  do{
+	i++;
   //tone(pin, frequency)=> Execute a sound with the frecuency per the pin that I passed
-tone(buzzer, 600);
-delay (3000); //Time that I will execute the last line of code
+tone(buzzer, 450);
+delay (200); //Time that I will execute the last line of code
 //Stop the signal > sound for the time that I will specific in the delay
 noTone(buzzer);
-delay (1000); //Time that I will execute the las time of code
-// END CODE BUZZER  
+delay (200); //Time that I will execute the las time of code
+  
+  }while(i<2);
+ delay(3000);
+	// END CODE BUZZER
 
-
+*/
 
 
 }
